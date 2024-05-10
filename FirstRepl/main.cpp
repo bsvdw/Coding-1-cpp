c5bf5089a4d87111d34a3169a9607987202d7850
bsvdw <35863521+bsvdw@users.noreply.github.com>
2024-05-10 01:00:47 -0500

    Added my first Repl

---
+++ FirstRepl/main.cpp
@@ -0,0 +1,13 @@
+// Braden VanderWeele Spring 2024
+// Coding 1, Week 1
+
+// comment this way or
+// select text and press Ctrl+/ to comment out
+/* block comment */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello Braden!\n";    // "\n" means "new line"
+}
